5ad19e504894a9fd589feee71a8452be3c76a337
Ruby-chan 3 <emrex1210@gmail.com>
2023-01-21 14:21:44 +0100

    fixed build script

--- build_de.bat
+++ build_de.bat
@@ -1,6 +1,9 @@
-set PAKPACK="Z:\Game Modding\Persona Modding\Multi-Purpose Tools\PAKTools\PAKPack.exe"
-set COMPILER="Z:\Game Modding\Persona Modding\Multi-Purpose Tools\AtlusScriptToolchain\AtlusScriptCompiler.exe"
+set PAKPACK="path to PAKPack.exe"
+set COMPILER="path to AtlusScriptCompiler.exe"
 set OUTPUT_PATH="DE_FINAL_FILES"
+
+if not exist %OUTPUT_PATH%\SCRIPT\FIELD mkdir %OUTPUT_PATH%\SCRIPT\FIELD
+if not exist %OUTPUT_PATH%\FIELD\ETC mkdir %OUTPUT_PATH%\FIELD\ETC
 
 %COMPILER% DE_FILES\DE_FIELD_hook.flow    -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\FIELD\ETC\FIELD.BF" -Hook
 %COMPILER% DE_FILES\DE_DUNGEON_hook.flow -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\FIELD\ETC\DUNGEON.BF" -Hook
