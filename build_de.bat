d1eb66b1f32f46ca4c55cc1c3289000c89b10231
Ruby-chan 3 <emrex1210@gmail.com>
2023-01-21 14:22:45 +0100

    nur ne kleinigkeit

--- build_de.bat
+++ build_de.bat
@@ -8,6 +8,6 @@ if not exist %OUTPUT_PATH%\FIELD\ETC mkdir %OUTPUT_PATH%\FIELD\ETC
 %COMPILER% DE_FILES\DE_FIELD_hook.flow    -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\FIELD\ETC\FIELD.BF" -Hook
 %COMPILER% DE_FILES\DE_DUNGEON_hook.flow -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\FIELD\ETC\DUNGEON.BF" -Hook
 %COMPILER% DE_FILES\DE_AT_DNG_hook.flow -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\FIELD\ETC\AT_DNG.BF" -Hook
-%COMPILER% DE_FILES\DE_FSCR0150_002_100_hook.flow -Compile -OutFormat V3BE -Library P5 -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\SCRIPT\FIELD\FSCR0150_002_100.BF" -Hook
+%COMPILER% DE_FILES\DE_FSCR0150_002_100_hook.flow -Compile -OutFormat V3BE -Library P5R -Encoding P5R_EFIGS -Out "%OUTPUT_PATH%\SCRIPT\FIELD\FSCR0150_002_100.BF" -Hook
 
 pause
